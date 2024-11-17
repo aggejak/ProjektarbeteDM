2ebc0f8e1e2c780a7ae176b898b4188ee5ef0240
MimmAm1 <williamlundgrenj@gmail.com>
2024-11-17 19:17:34 +0100

    left right movement
    added left right movement in playercontroller

--- ProjektarbeteDigitalaMedier/Assets/Scripts/PlayerController.cs
+++ ProjektarbeteDigitalaMedier/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Transform feetTransform;
-  
+    public float speed = 5;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        
     }
+
     private void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement along the x-axis in 2D
+        Vector2 horizontalMove = new Vector2(horizontalInput * speed * horizontalMultiplier * Time.fixedDeltaTime, 0);
+
+        // Update the rigidbody position
+        rb.MovePosition(rb.position + horizontalMove);
     }
 }
