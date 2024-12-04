3ca3267ba7d44cc5eeb7dc888afe81dd5a842e5d
MimmAm1 <williamlundgrenj@gmail.com>
2024-12-04 15:31:04 +0100

    Audiosource
    All'm sayin i s bomboclat!

---
+++ ProjektarbeteDigitalaMedier/Assets/Scripts/CollisionAudio.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CollisionAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+
+private void OnTriggerEnter(Collider collision){
+audioSource.PlayOneShot(audioClip);
+}
+
+}
+
+
